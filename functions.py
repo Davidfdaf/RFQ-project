dd92429ba8d3616e982a2bc457693a05fbdbaa89
YeonsooJee <yeonsoojee@dhcp-vl2052-1108.redrover.cornell.edu>
2022-11-18 14:59:04 -0500

    hi

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ def max_pnl_price(bonderror,trade,column1,column2,model1,model2,penalty):
     if trade['Side']==1:
         sample['delta_from_mid'] = np.arange(min(0.1,cut_off-bonderror),max(0.1,cut_off-bonderror),0.01)
     if trade['Side']==-1:
-        sample['delta_from_mid'] = np.arange(min(cut_off+bonderror),max(cut_off+bonderror),1.2,0.01)
+        sample['delta_from_mid'] = np.arange(min(cut_off+bonderror,1.2),max(cut_off+bonderror,1.2),0.01)
     
     sample[column1] = trade[column1]
 
@@ -23,12 +23,12 @@ def pnl(quote,test):
     test_perf['My_Price']=quote
     test_perf[['Side','QuotedPrice','NextMidPrice']]=test[['Side','QuotedPrice','NextMidPrice']]
     
-    pos_pnl = len(test_perf[(test_perf['Side'] == 1)&(test_perf['NextMidPrice']>test_perf['My_Price'])]) + len(test_perf[(test_perf['Side'] == -1)&(test_perf['NextMidPrice']<test_perf['My_Price'])])
+    pos_pnl = len(test_perf[(test_perf['Side'] == 'Bid')&(test_perf['NextMidPrice']>test_perf['My_Price'])]) + len(test_perf[(test_perf['Side'] == 'Offer')&(test_perf['NextMidPrice']<test_perf['My_Price'])])
     
-    better_price = len(test_perf[(test_perf['Side'] == 1)&(test_perf['QuotedPrice']<test_perf['My_Price'])]) + len(test_perf[(test_perf['Side'] == -1)&(test_perf['QuotedPrice']>test_perf['My_Price'])])
+    better_price = len(test_perf[(test_perf['Side'] == 'Bid')&(test_perf['QuotedPrice']<test_perf['My_Price'])]) + len(test_perf[(test_perf['Side'] == 'Offer')&(test_perf['QuotedPrice']>test_perf['My_Price'])])
     
-    traded_pos_pnl = len(test_perf[(test_perf['Side'] == 1)&(test_perf['NextMidPrice']>test_perf['My_Price'])&(test_perf['QuotedPrice']<test_perf['My_Price'])]) + len(test_perf[(test_perf['Side'] == -1)&(test_perf['NextMidPrice']<test_perf['My_Price'])&(test_perf['QuotedPrice']>test_perf['My_Price'])])
-    
+    traded_pos_pnl = len(test_perf[(test_perf['Side'] == 'Bid')&(test_perf['NextMidPrice']>test_perf['My_Price'])&(test_perf['QuotedPrice']<test_perf['My_Price'])]) + len(test_perf[(test_perf['Side'] == 'Offer')&(test_perf['NextMidPrice']<test_perf['My_Price'])&(test_perf['QuotedPrice']>test_perf['My_Price'])])
+
     print("pos_pnl:%.0f"%(pos_pnl))
     print("better_price:%.0f"%(better_price))
     print("traded_pos_pnl:%.0f"%(traded_pos_pnl))
